975e504d6ea565dcf3cf28d59780776d475ec15a
osamashokry03 <osama.shokry.03@gmail.com>
2026-06-12 12:02:39 +0300

    Update sqoop_list_dbs.sh

--- scripts/sqoop_list_dbs.sh
+++ scripts/sqoop_list_dbs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 /home/asf/bigdata/sqoop-1.4.7.bin__hadoop-2.6.0/bin/sqoop list-databases \
   --connect 'jdbc:mysql://192.168.116.1:3306/' \
-  --username 'sqoop' \
-  --password 'Mm@6655Mm'
+  --username '******' \
+  --password '*******'
